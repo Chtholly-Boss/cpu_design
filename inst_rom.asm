8fdb8e369ab7037cf5ca032f5227d39086d0e8c9
Chtholly-Boss <220110915@stu.hit.edu.cn>
2024-02-16 23:04:53 +0800

    chapter 9 final test added

--- inst_rom.asm
+++ inst_rom.asm
@@ -7,26 +7,27 @@ Disassembly of section .text:
 00000000 <_start>:
    0:	34011234 	li	at,0x1234
    4:	ac010000 	sw	at,0(zero)
-   8:	34015678 	li	at,0x5678
-   c:	e0010000 	sc	at,0(zero)
+   8:	34021234 	li	v0,0x1234
+   c:	34010000 	li	at,0x0
   10:	8c010000 	lw	at,0(zero)
-  14:	00000000 	nop
-  18:	34010000 	li	at,0x0
-  1c:	c0010000 	ll	at,0(zero)
+  14:	10220003 	beq	at,v0,24 <Label>
+  18:	00000000 	nop
+  1c:	34014567 	li	at,0x4567
   20:	00000000 	nop
-  24:	20210001 	addi	at,at,1
-  28:	e0010000 	sc	at,0(zero)
-  2c:	8c010000 	lw	at,0(zero)
 
-00000030 <_loop>:
-  30:	0800000c 	j	30 <_loop>
-  34:	00000000 	nop
+00000024 <Label>:
+  24:	340189ab 	li	at,0x89ab
+  28:	00000000 	nop
+
+0000002c <_loop>:
+  2c:	0800000b 	j	2c <_loop>
+  30:	00000000 	nop
 	...
 
 Disassembly of section .reginfo:
 
 00000040 <.reginfo>:
-  40:	00000002 	srl	zero,zero,0x0
+  40:	00000006 	srlv	zero,zero,zero
 	...
 
 Disassembly of section .MIPS.abiflags:
@@ -42,6 +43,6 @@ Disassembly of section .gnu.attributes:
 
 00000000 <.gnu.attributes>:
    0:	41000000 	0x41000000
-   4:	0f676e75 	jal	d9db9d4 <_loop+0xd9db9a4>
+   4:	0f676e75 	jal	d9db9d4 <_loop+0xd9db9a8>
    8:	00010000 	sll	zero,at,0x0
    c:	00070401 	0x70401
